82e65a59d444f45e087f6843e145c081b0c725b7
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-07-17 16:21:42 +0200

    add chip component for state description

---
+++ front/src/react/components/FileUIContainer/StateChip/StateChip.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {CANCELLED, DONE, ERROR} from "../../../../redux/uploadStateTypes";
+import Chip from "@material-ui/core/Chip";
+import style from './StateChip.module.css';
+
+class StateChip extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = { showStateLog: false }
+    }
+
+    handleClick() {
+        this.setState({
+            ...this.state,
+            showStateLog: !this.state.showStateLog
+        });
+    }
+
+    render() {
+        if (this.props.uploadState === DONE) {
+            return <Chip color="primary" style={{backgroundColor: 'green'}} label="Done"/>;
+        } else if (this.props.uploadState === ERROR) {
+            return (
+                <div>
+                    <Chip color="primary" style={{backgroundColor: 'red'}} label="Error" onClick={this.handleClick}/>
+                    { this.state.showStateLog &&
+                        <ul className={style.logContainer}>
+                            <li className={style.logReasonEntry}>{ this.props.stateLog.reason }</li>
+                            <li className={style.logStatusEntry}>{ this.props.stateLog.status}</li>
+                        </ul>
+                    }
+                </div>
+               );
+        } else if (this.props.uploadState === CANCELLED) {
+            return <Chip color="primary" style={{backgroundColor: 'orange'}} label="Cancelled"/>
+        }
+        return <div/>;
+    }
+}
+
+export default StateChip;
